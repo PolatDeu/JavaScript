4eeb54b5297cee5dcacbfc30d7c476aeabcc73f1
PolatDeu <ahmetpolat@mein.gmx>
2021-07-11 21:00:12 +0200

    ..

---
+++ Calculator/script.js
@@ -0,0 +1,8 @@
+const input1Value = document.getElementById("inputunten").value;
+console.log(input1Value);
+
+const btn7 = document.getElementById("btn5");
+btn7.onclick=function num (){
+    console.log(input1Value);
+    input1Value.innerText = `yapma`
+}
